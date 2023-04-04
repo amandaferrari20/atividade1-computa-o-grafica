7f3ff731ac641c078f2734805f237f0a79bcae3b
Iago Carmona <iagoortegacarmona@gmail.com>
2023-04-04 18:18:01 -0300

    Trabalho finalizado

--- main.c
+++ main.c
@@ -1,23 +1,27 @@
 #include <GL/glut.h>
 #include <stdio.h>
+#include <string.h>
 
 float xPos[4] = {10.0, 20.0, 20.0, 10.0};
 float yPos[4] = {10.0, 10.0, 20.0, 20.0};
 float centroid[2] = {5.0, 5.0};
 
 float angulo = 5.0;
-float scale = 1.1;
+float scaleUp = 1.1;
+float scaleDown = 0.9;
 
 float angleRotated = 0.0;
 
 unsigned char mode = 't';
 
-void calcularCentroide(float *xPos, float *yPos);
-void display(void);
 int init(void);
-void desenha_quadrado(void);
+void display(void);
 void teclas_normais(unsigned char tecla, int x, int y);
 void teclas_especiais(int tecla, int x, int y);
+void desenha_quadrado(void);
+void calcularCentroide(float *xPos, float *yPos);
+void doRotate(float angle, char direction);
+void doScale(float scale);
 
 
 int main(int argc, char **argv){
@@ -71,78 +75,125 @@ void teclas_especiais(int tecla, int x, int y){
     // Se o mode for T, as setas fazem a translação para o local desejado
     if (mode == 't'){ 
         if (tecla == GLUT_KEY_RIGHT){   
-            float new_angle = angleRotated * -1;
-            glRotatef(angleRotated, 0.0, 0.0, 1.0);
-            glTranslatef(1.0f, 0.0f, 0.0f);
-            glRotatef(new_angle, 0.0, 0.0, 1.0);
-            for(int i = 0; i < 4; i++){
-                xPos[i] += 1;
+            if(angleRotated != 0){
+                if(angleRotated < 0){
+                    calcularCentroide(xPos, yPos);
+                    glTranslatef(centroid[0], centroid[1], 0);
+                    glRotatef(angleRotated * -1, 0.0, 0.0, 1.0);
+                    glTranslatef(-centroid[0], -centroid[1], 0);
+
+                    glTranslatef(1.0f, 0.0f, 0.0f);
+                    
+                    glTranslatef(centroid[0], centroid[1], 0);
+                    glRotatef(angleRotated, 0.0, 0.0, 1.0);
+                    glTranslatef(-centroid[0], -centroid[1], 0);
+                } else {
+                    doRotate((angleRotated * -1), 'r');
+                    glTranslatef(1.0f, 0.0f, 0.0f);
+                    doRotate(angleRotated, 'l');
+                }
+            } else {
+                glTranslatef(1.0f, 0.0f, 0.0f);
+                for(int i = 0; i < 4; i++){
+                    xPos[i] += 1;
+                }
             }
         }
         if (tecla == GLUT_KEY_LEFT){
-            float new_angle = angleRotated * -1;
-            glRotatef(angleRotated, 0.0, 0.0, 1.0);
-            glTranslatef(-1.0f, 0.0f, 0.0f);
-            glRotatef(new_angle, 0.0, 0.0, 1.0);
-            for(int i = 0; i < 4; i++){
-                xPos[i] -= 1;
+            if(angleRotated != 0){
+                calcularCentroide(xPos, yPos);
+                glTranslatef(centroid[0], centroid[1], 0);
+                glRotatef(angleRotated * -1, 0.0, 0.0, 1.0);
+                glTranslatef(-centroid[0], -centroid[1], 0);
+
+                glTranslatef(-1.0f, 0.0f, 0.0f);
+
+                glTranslatef(centroid[0], centroid[1], 0);
+                glRotatef(angleRotated, 0.0, 0.0, 1.0);
+                glTranslatef(-centroid[0], -centroid[1], 0);
+            } else {
+                glTranslatef(-1.0f, 0.0f, 0.0f);
+                for(int i = 0; i < 4; i++){
+                    xPos[i] -= 1;
+                }
             }
         }
         if (tecla == GLUT_KEY_UP){
-            float new_angle = angleRotated * -1;
-            glRotatef(angleRotated, 0.0, 0.0, 1.0);
-            glTranslatef(0.0f, 1.0f, 0.0f);
-            glRotatef(new_angle, 0.0, 0.0, 1.0);
-            for(int i = 0; i < 4; i++){
-                yPos[i] += 1;
+            if(angleRotated != 0){
+                calcularCentroide(xPos, yPos);
+                glTranslatef(centroid[0], centroid[1], 0);
+                glRotatef(angleRotated * -1, 0.0, 0.0, 1.0);
+                glTranslatef(-centroid[0], -centroid[1], 0);
+
+                glTranslatef(0.0f, 1.0f, 0.0f);
+
+                glTranslatef(centroid[0], centroid[1], 0);
+                glRotatef(angleRotated, 0.0, 0.0, 1.0);
+                glTranslatef(-centroid[0], -centroid[1], 0);
+            } else {
+                glTranslatef(0.0f, 1.0f, 0.0f);
+                for(int i = 0; i < 4; i++){
+                    yPos[i] += 1;
+                }
             }
         }
         if (tecla == GLUT_KEY_DOWN){
-            float new_angle = angleRotated * -1;
-            glRotatef(angleRotated, 0.0, 0.0, 1.0);
-            glTranslatef(0.0f, -1.0f, 0.0f);
-            glRotatef(new_angle, 0.0, 0.0, 1.0);
-            for(int i = 0; i < 4; i++){
-                yPos[i] -= 1;
+            if(angleRotated != 0) {
+                calcularCentroide(xPos, yPos);
+                glTranslatef(centroid[0], centroid[1], 0);
+                glRotatef(angleRotated * -1, 0.0, 0.0, 1.0);
+                glTranslatef(-centroid[0], -centroid[1], 0);
+
+                glTranslatef(0.0f, -1.0f, 0.0f);
+
+                glTranslatef(centroid[0], centroid[1], 0);
+                glRotatef(angleRotated, 0.0, 0.0, 1.0);
+                glTranslatef(-centroid[0], -centroid[1], 0);
+            } else {
+                glTranslatef(0.0f, -1.0f, 0.0f);
+                for(int i = 0; i < 4; i++){
+                    yPos[i] -= 1;
+                }
             }
         }
     }
 
     // Se o mode for S, as setas para cima e para baixo relizam a escala
     if (mode == 's'){ 
-        if (tecla == GLUT_KEY_UP){
-            calcularCentroide(xPos, yPos);
-            glTranslatef(centroid[0], centroid[1], 0);
-            glScalef(scale, scale, 1.0);
-            glTranslatef(-centroid[0], -centroid[1], 0);
-        }
-        if (tecla == GLUT_KEY_DOWN){
-            calcularCentroide(xPos, yPos);
-            float new_scale = 0.9;
-            glTranslatef(centroid[0], centroid[1], 0);
-            glScalef(new_scale, new_scale, 1.0);
-            glTranslatef(-centroid[0], -centroid[1], 0);
-        }
+        if (tecla == GLUT_KEY_UP) doScale(scaleUp);
+        if (tecla == GLUT_KEY_DOWN) doScale(scaleDown);
     }
 
     // Se o mode for R, as setas de esquerda e direira realizam a rotação
     if (mode == 'r'){ 
-        if (tecla == GLUT_KEY_LEFT){
-            calcularCentroide(xPos, yPos);
-            glTranslatef(centroid[0], centroid[1], 0);
-            glRotatef(angulo, 0.0, 0.0, 1.0);
-            glTranslatef(-centroid[0], -centroid[1], 0);
-            angleRotated += angulo;
-        }
-        if (tecla == GLUT_KEY_RIGHT){
-            calcularCentroide(xPos, yPos);
-            glTranslatef(centroid[0], centroid[1], 0);
-            glRotatef(-angulo, 0.0, 0.0, 1.0);
-            glTranslatef(-centroid[0], -centroid[1], 0);
-            angleRotated -= angulo;
-        }
+        if (tecla == GLUT_KEY_LEFT) doRotate(angulo, 'l');
+        if (tecla == GLUT_KEY_RIGHT) doRotate(-angulo, 'r');
     }
     glutPostRedisplay();
+}
+
+void doScale(float scale){
+    calcularCentroide(xPos, yPos);
+    glTranslatef(centroid[0], centroid[1], 0);
+    glScalef(scale, scale, 1.0);
+    glTranslatef(-centroid[0], -centroid[1], 0);
+}
+
+void doRotate(float angle, char direction){
+    calcularCentroide(xPos, yPos);
+    glTranslatef(centroid[0], centroid[1], 0);
+    glRotatef(angle, 0.0, 0.0, 1.0);
+    glTranslatef(-centroid[0], -centroid[1], 0);
+    if(direction == 'r') {
+        angleRotated -= (angle * (-1));
+        printf("%.2f\n", angleRotated);
+        if(angleRotated == -360.0) angleRotated = 0.0;
+    }
+    if(direction == 'l') {
+        angleRotated += angle; 
+        printf("%.2f\n", angleRotated);
+        if(angleRotated == 360.0) angleRotated = 0.0;
+    }
 }
 
 void calcularCentroide(float *xPos, float *yPos) {
